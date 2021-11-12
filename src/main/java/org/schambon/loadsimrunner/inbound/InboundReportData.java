449251a9d6f3848cf0cfcfcab80aa0fe4e42c3f5
Sylvain Chambon <sylvain.chambon@mongodb.com>
2021-11-12 16:24:17 +0100

    ignore unknow inbound report props

--- src/main/java/org/schambon/loadsimrunner/inbound/InboundReportData.java
+++ src/main/java/org/schambon/loadsimrunner/inbound/InboundReportData.java
@@ -1,7 +1,9 @@
 package org.schambon.loadsimrunner.inbound;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class InboundReportData {
     
     @JsonProperty("ops")
